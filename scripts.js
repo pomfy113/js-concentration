8e4fd3ab7bb8d65ce75a30967479988c87b43ffb
pomfy113 <pomfy113@gmail.com>
2017-09-16 23:14:45 -0700

    Finished - Start on Improvements

--- scripts.js
+++ scripts.js
@@ -1,9 +1,37 @@
 var resetButton = document.getElementById("reset-button");
 var gameSquares = [];
+var firstSquare = null;
+
 
 var colors = [];
 for (var i = 0; i < 10; i++) {
   colors.push('square-' + i);
+}
+
+function random(n) {
+  return Math.floor(Math.random() * n);
+}
+
+function getSomeColors() {
+  var colorscopy = colors.slice();
+  var randomColors = [];
+  for (var i = 0; i < 8; i++) {
+    var index = random(colorscopy.length);
+    randomColors.push(colorscopy.splice(index, 1)[0]);
+  }
+  return randomColors.concat(randomColors.slice());
+}
+
+function randomizeColors() {
+  var randomColors = getSomeColors();
+  gameSquares.forEach(function(gameSquare) {
+    var color = randomColors.splice(random(randomColors.length), 1)[0];
+    gameSquare.setColor(color);
+  });
+}
+
+resetButton.onclick = function(){
+    clearGame();
 }
 
 function GameSquare(el, color) {
@@ -15,7 +43,7 @@ function GameSquare(el, color) {
   }
 
 
-    GameSquare.prototype.handleEvent = function(e) {
+  GameSquare.prototype.handleEvent = function(e) {
     switch (e.type) {
       case "click":
         if (this.isOpen || this.isLocked) {
@@ -23,8 +51,9 @@ function GameSquare(el, color) {
         }
         this.isOpen = true;
         this.el.classList.add('flip');
-        }
+        checkGame(this); // <- check the game here!
     }
+  }
 
     GameSquare.prototype.reset = function() {
       this.isOpen = false;
@@ -45,10 +74,44 @@ function GameSquare(el, color) {
 
 
 function setupGame() {
-  var array = document.getElementsByClassName("game-square");
-  for (var i = 0; i < array.length; i++) {
-    gameSquares.push(new GameSquare(array[i], colors[0]));
-  }
+      var array = document.getElementsByClassName("game-square");
+      var randomColors = getSomeColors();             // Get an array of 8 random color pairs
+      for (var i = 0; i < array.length; i++) {
+        var index = random(randomColors.length);      // Get a random index
+        var color = randomColors.splice(index, 1)[0]; // Get the color at that index
+        // Use that color to initialize the GameSquare
+        gameSquares.push(new GameSquare(array[i], color));
+    }
 }
+
+function checkGame(gameSquare) {
+      if (firstSquare === null) {
+        firstSquare = gameSquare;
+        return
+      }
+
+      if (firstSquare.color === gameSquare.color) {
+        firstSquare.lock();
+        gameSquare.lock();
+      } else {
+        var a = firstSquare;
+        var b = gameSquare;
+        setTimeout(function() {
+          a.reset();
+          b.reset();
+          firstSquare = null;
+        }, 400);
+      }
+      firstSquare = null;
+    }
+
+    function clearGame() {
+      gameSquares.forEach(function(gameSquare) {
+        gameSquare.reset();
+      });
+      setTimeout(function() {
+        randomizeColors();
+      }, 500);
+    }
 
 setupGame();
